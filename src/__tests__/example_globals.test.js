66ed751cc7accfa5882ba8a16fcd6a20585f86d2
Leonardo Omaña <leotangram@gmail.com>
2021-04-15 13:02:07 -0500

    Jest - principal global variables

---
+++ src/__tests__/example_globals.test.js
@@ -0,0 +1,12 @@
+describe("this is for group test cases", () => {
+  test("description", () => {
+    expect(true).toBe(true);
+  });
+  test("description 2", () => {
+    expect(true).toBe(true);
+  });
+
+  test("description 3", () => {
+    expect(true).toBe(true);
+  });
+});
