0aaa907f907079b9ba4e30fc7c7baebf8cf89897
Leonardo Omaña <jose.omana@rappi.com>
2021-04-20 15:32:56 -0500

    How to apply mocks to your own modules

--- src/__tests__/example_mocks.test.js
+++ src/__tests__/example_mocks.test.js
@@ -1,3 +1,8 @@
+import { storage } from '../lib/storage'
+import { getUsername, saveUsername } from '../user'
+
+jest.mock('../lib/storage')
+
 describe('mocks', () => {
   test('first example', () => {
     const myMock = jest
@@ -16,5 +21,25 @@ describe('mocks', () => {
     expect(result1).toBe(true)
     expect(result2).toBe('Hello World')
     expect(result3).toBe(5)
+  })
+  test('second example', () => {
+    const username = 'john doe'
+    saveUsername(username)
+    expect(storage.save).toHaveBeenCalled()
+    expect(storage.save).toHaveBeenCalledTimes(1)
+    expect(storage.save).toHaveBeenCalledWith({
+      key: 'username',
+      value: username
+    })
+  })
+  test('third example', () => {
+    const username = 'john doe'
+    storage.get.mockReturnValueOnce(username)
+
+    const result = getUsername()
+
+    expect(result).toBe(username)
+    expect(storage.get).toHaveBeenCalledTimes(1)
+    expect(storage.get).toHaveBeenCalledWith({ key: 'username' })
   })
 })
